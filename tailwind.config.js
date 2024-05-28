433ffe71651f7a0b85fb6af7f052dbf1eb1fdee5
Jeff <you@example.com>
2024-05-28 04:10:32 +0300

    Added Custom Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./Views/**/*.{cshtml,html}", "./Areas/**/*.{cshtml,html}"],
   theme: {
     extend: {},
+    colors: {
+      "primary-color": "#E6D7B8",
+      "secondary-color": "#FFEECA",
+      "tertiary-color": "#C0462F",
+      "primary-light": "#FFFFFF",
+      "primary-dark": "#101010",
+      "secondary-dark": "#000000",
+    },
   },
   plugins: [],
 };
