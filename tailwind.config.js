b83e2bfac427e600316629cb4e141bbdd666bbf3
Jeff <you@example.com>
2024-05-28 04:07:30 +0300

    Configured Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./Views/**/*.{cshtml,html}", "./Areas/**/*.{cshtml,html}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
